07a13639caedaa5f2581eb61cbf3c348fec734a6
yosefalemu <yosefalemu007@gmail.com>
2025-07-11 18:40:04 +0300

    feat: implement theme setup with shadcn
    - Added shadcn theme configuration for light and dark modes
    - Integrated theme toggle component
    - Updated CSS variables for consistent styling
    - Configured Tailwind for theme support

---
+++ src/app/(dashboard)/settings/me/page.tsx
@@ -0,0 +1,65 @@
+"use client";
+import { LogOut } from "lucide-react";
+
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { useCurrentUser } from "@/features/auth/api/current-api";
+import { useLogout } from "@/features/auth/api/logout-api";
+
+export default function ProfileSettings() {
+  const logoutMutation = useLogout();
+  const router = useRouter();
+  const { setTheme, resolvedTheme } = useTheme();
+  const [themeChecked, setThemeChecked] = useState<boolean>(
+    resolvedTheme === "dark"
+  );
+  const {} = useCurrentUser();
+
+  useEffect(() => {
+    setThemeChecked(resolvedTheme === "dark");
+  }, [resolvedTheme]);
+
+  console.log("theme detected", resolvedTheme);
+
+  const handleLogout = () => {
+    logoutMutation.mutate(undefined, {
+      onSuccess: () => {
+        toast.success("Logged out successfully");
+        router.push("/sign-in");
+      },
+      onError: () => {
+        toast.error("An error occured while logging out");
+      },
+    });
+  };
+  return (
+    <div className="flex space-x-4 h-full">
+      <div className="w-1/2 border-2 p-4">Column one</div>
+      <div className="w-1/2 flex flex-col justify-end border-2 p-4">
+        <div className="flex items-center space-x-2 px-4 py-2">
+          <Label htmlFor="airplane-mode">Dark Mode</Label>
+          <Switch
+            id="dark-mode"
+            checked={themeChecked}
+            onCheckedChange={() => {
+              const newTheme = themeChecked ? "light" : "dark";
+              setTheme(newTheme);
+              setThemeChecked(!themeChecked);
+            }}
+          />
+        </div>
+        <div
+          className="flex items-center space-x-2 px-4 py-2"
+          onCanPlay={handleLogout}
+        >
+          <LogOut className="size-4 mr-2" />
+          Log out
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/features/auth/components/user-button.tsx
+++ src/features/auth/components/user-button.tsx
@@ -1,84 +1,14 @@
-"use client";
-import DootedSeparator from "@/components/dooted-separator";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { LogOut } from "lucide-react";
-import { IoPersonOutline } from "react-icons/io5";
-import { useLogout } from "../api/logout-api";
-import { useCurrentUser } from "../api/current-api";
-import { toast } from "sonner";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function UserButton() {
-  const logoutMutation = useLogout();
-  const router = useRouter();
-  const { data, isLoading, isError } = useCurrentUser();
-
-  const handleLogout = () => {
-    logoutMutation.mutate(undefined, {
-      onSuccess: () => {
-        toast.success("Logged out successfully");
-        router.push("/sign-in");
-      },
-      onError: () => {
-        toast.error("An error occured while logging out");
-      },
-    });
-  };
   return (
-    <>
-      {isLoading ? (
-        <div className="bg-neutral-200 font-medium rounded-full size-10 overflow-hidden flex items-center justify-center">
-          <IoPersonOutline className="text-neutral-500 text-lg" />
-        </div>
-      ) : isError ? (
-        <div>TODO::HANDLE ERROR</div>
-      ) : data ? (
-        <DropdownMenu modal={true}>
-          <DropdownMenuTrigger className="outline-none relative">
-            <Avatar className="size-10 hover:opacity-75 transition border border-neutral-300">
-              <AvatarFallback className="bg-neutral-200 font-medium text-neutral-500 flex items-center justify-center">
-                Y
-              </AvatarFallback>
-            </Avatar>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent
-            align="end"
-            side="bottom"
-            className="w-60"
-            sideOffset={10}
-          >
-            <div className="flex flex-col items-center justify-center gap-2 px-2.5 py-4">
-              <Avatar className="size-[52px] border border-neutral-300">
-                <AvatarFallback className="bg-neutral-200 text-xl font-medium text-neutral-500 flex items-center justify-center">
-                  Y
-                </AvatarFallback>
-              </Avatar>
-              <div className="flex flex-col items-center justify-center gap-1">
-                <p className="text-sm font-medium text-neutral-900">Y</p>
-                <p className="text-sm text-neutral-500">Y</p>
-              </div>
-            </div>
-            <DootedSeparator className="mb-2" />
-            <DropdownMenuItem
-              className="h-10 flex items-center justify-center text-amber-700 focus:text-amber-900 focus:bg-transparent font-medium cursor-pointer"
-              onClick={() => handleLogout()}
-            >
-              <LogOut className="size-4 mr-2" />
-              Log out
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      ) : (
-        <div className="bg-neutral-200 font-medium rounded-full size-10 overflow-hidden flex items-center justify-center">
-          <IoPersonOutline className="text-neutral-500 text-lg" />
-        </div>
-      )}
-    </>
+    <Link href="/settings/me">
+      <Avatar className="size-[52px] border border-neutral-300">
+        <AvatarFallback className="bg-neutral-200 text-xl font-medium text-neutral-500 flex items-center justify-center">
+          Y
+        </AvatarFallback>
+      </Avatar>
+    </Link>
   );
 }
