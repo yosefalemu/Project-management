ef55302ce4688653abf9a45addded7ae4472db8e
Yosef Alemu <yosefalemu007@gmail.com>
2025-08-03 20:46:53 +0300

    feat(zustand-persist): Implement zustand-persist - configure zustand persist - add zustand persist to the profile-view section

--- src/states/user-profile.ts
+++ src/states/user-profile.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 type UserProfileState = {
   isOpen: boolean;
@@ -7,9 +8,18 @@ type UserProfileState = {
   toggleUserProfile: () => void;
 };
 
-export const userProfileViewStore = create<UserProfileState>((set) => ({
-  isOpen: false,
-  openUserProfile: () => set({ isOpen: true }),
-  closeUserProfile: () => set({ isOpen: false }),
-  toggleUserProfile: () => set((state) => ({ isOpen: !state.isOpen })),
-}));
+export const userProfileViewStore = create<UserProfileState>()(
+  persist(
+    (set) => ({
+      isOpen: false,
+      openUserProfile: () => set({ isOpen: true }),
+      closeUserProfile: () => set({ isOpen: false }),
+      toggleUserProfile: () => set((state) => ({ isOpen: !state.isOpen })),
+    }),
+    {
+      name: "user-profile-view",
+      storage: createJSONStorage(() => localStorage),
+      partialize: (state) => ({ isOpen: state.isOpen }),
+    }
+  )
+);
