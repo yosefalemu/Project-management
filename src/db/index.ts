bc7e75a20ba902a29adbbeed92d650522254e8e5
Yosef Alemu <125744573+yosefalemu@users.noreply.github.com>
2025-08-22 19:55:52 +0300

    Update index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,30 +1,30 @@
 // Comment out when using Neon with drizzle-orm/neon-http
-// import { drizzle } from "drizzle-orm/neon-http";
-// import { neon } from "@neondatabase/serverless";
-// import { config } from "dotenv";
-
-// config({ path: ".env.local" });
-
-// const sql = neon(process.env.DATABASE_URL!);
-
-// const db = drizzle(sql, { logger: true });
-
-// export { db };
-
-
-import { Pool, neonConfig } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
 import { config } from "dotenv";
-import ws from "ws";
-import * as schema from "./schema/schema";
-
-neonConfig.webSocketConstructor = ws;
 
 config({ path: ".env.local" });
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is not defined");
-}
+const sql = neon(process.env.DATABASE_URL!);
+
+const db = drizzle(sql, { logger: true });
+
+export { db };
+
+
+// import { Pool, neonConfig } from "@neondatabase/serverless";
+// import { drizzle } from "drizzle-orm/neon-serverless";
+// import { config } from "dotenv";
+// import ws from "ws";
+// import * as schema from "./schema/schema";
+
+// neonConfig.webSocketConstructor = ws;
+
+// config({ path: ".env.local" });
+
+// if (!process.env.DATABASE_URL) {
+//   throw new Error("DATABASE_URL is not defined");
+// }
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
