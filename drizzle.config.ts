46fd0797a40b60e968d01c8a89aea57e98687ddc
Yosef Alemu <yosefalemu007@gmail.com>
2025-08-23 13:10:49 +0300

    change drizzle-config file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -19,6 +19,10 @@ for (const [key, value] of Object.entries(requiredEnvVars)) {
     throw new Error(`Missing required environment variable: ${key}`);
   }
 }
+const ssl =
+  process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: true, ca: readFileSync("./ca.pem").toString() }
+    : false;
 
 export default defineConfig({
   dialect: "postgresql",
@@ -30,9 +34,6 @@ export default defineConfig({
     host: process.env.DB_HOST as string,
     port: Number(process.env.DB_PORT),
     database: process.env.DB_NAME as string,
-    ssl: {
-      rejectUnauthorized: true,
-      ca: readFileSync("./ca.pem").toString(),
-    },
+    ssl,
   },
 });
