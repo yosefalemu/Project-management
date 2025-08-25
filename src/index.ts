1ca9b495456c1c724dac95fd93b9210dc3b2f6a7
Yosef Alemu <yosefalemu007@gmail.com>
2025-08-25 16:56:35 +0300

    feat(db): setup database connection - change the db url for development - fix the not load .env.local variables

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-import "dotenv/config";
-import { drizzle } from "drizzle-orm/node-postgres";
+import * as dotenv from "dotenv";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+import * as schema from "./db/schema";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+dotenv.config({ path: ".env.local" });
+
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql, { schema });
